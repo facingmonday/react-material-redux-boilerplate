9acb5ffb51b2dfa487e615f5221dc0eb4e7e352d
Jason Price <jasonprice@Jasons-MacBook-Pro.local>
2019-08-06 09:04:10 -0400

    Fixing linting errors

--- server.js
+++ server.js
@@ -2,17 +2,19 @@ const express = require('express');
 const cors = require('cors');
 const morgan = require('morgan');
 
-require('dotenv').config()
+require('dotenv').config();
 
 const app = express();
 
 app.use(morgan());
-app.use(cors({
-  origin: ['http://localhost:8080'],
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],
-  allowedHeaders: ['Content-Type', 'Authorization'],
-  credentials: true,
-}));
+app.use(
+  cors({
+    origin: ['http://localhost:8080'],
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true,
+  }),
+);
 
 app.get('/users/me', (req, res) => {
   if (req.headers.authorization) {
@@ -24,36 +26,42 @@ app.get('/users/me', (req, res) => {
   return res.sendStatus(401);
 });
 
-app.get('/auth/local', (req, res) => res.json({
-  name: 'Test User',
-  email: 'test@test.com',
-  token: 'dfhafoiaudfalksfhalkdf',
-}));
+app.get('/auth/local', (req, res) =>
+  res.json({
+    name: 'Test User',
+    email: 'test@test.com',
+    token: 'dfhafoiaudfalksfhalkdf',
+  }),
+);
 
-app.post('/auth/local', (req, res) => res.json({
-  name: 'Test User',
-  email: 'test@test.com',
-  token: 'dfhafoiaudfalksfhalkdf',
-}));
+app.post('/auth/local', (req, res) =>
+  res.json({
+    name: 'Test User',
+    email: 'test@test.com',
+    token: 'dfhafoiaudfalksfhalkdf',
+  }),
+);
 
-app.get('/products', (req, res) => res.status(200).json({
-  stats: 2,
-  results: [
-    {
-      id: 1,
-      name: "My First Product",
-      price: 25.99,
-    },
-    {
-      id: 2,
-      name: "Another Product",
-      price: 12.50,
-    },
-  ],
-}));
+app.get('/products', (req, res) =>
+  res.status(200).json({
+    stats: 2,
+    results: [
+      {
+        id: 1,
+        name: 'My First Product',
+        price: 25.99,
+      },
+      {
+        id: 2,
+        name: 'Another Product',
+        price: 12.5,
+      },
+    ],
+  }),
+);
 
-app.get('/api/users', (req, res) => 
-  //return res.sendStatus(500);
+app.get('/api/users', (req, res) =>
+  // return res.sendStatus(500);
   res.status(200).json({
     stats: 1,
     results: [
@@ -64,10 +72,10 @@ app.get('/api/users', (req, res) =>
         email: 'test@test.com',
       },
     ],
-  })
+  }),
 );
 
-app.listen(process.env.SERVER_PORT, () => {  
+app.listen(process.env.SERVER_PORT, () => {
   console.log(`Dev API is running on port ${process.env.SERVER_PORT}...`);
 });
 
